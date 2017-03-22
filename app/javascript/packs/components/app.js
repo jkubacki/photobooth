2915322550be7e341b329716de7f13925ba3ed43
Jakub Kubacki <kubacki.jk@gmail.com>
2017-03-22 18:06:56 +0100

    Add webcam

--- app/javascript/packs/components/app.js
+++ app/javascript/packs/components/app.js
@@ -1,11 +1,12 @@
 import React from "react"
 import { connect } from "react-redux"
+import Booth from "./booth"
 
 class App extends React.Component {
   render() {
     return (
       <div>
-        Sample app
+        <Booth />
       </div>
     )
   }
---
+++ app/javascript/packs/components/booth.js
@@ -0,0 +1,12 @@
+import React from "react"
+import Webcam from "react-webcam"
+
+export default class Booth extends React.Component {
+  render() {
+    return (
+      <div>
+        <Webcam/>
+      </div>
+    )
+  }
+}
